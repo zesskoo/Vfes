a8d9eda605d84931ec51264f0b7c7c1f90f98d8f
zesskoo <sultanjanybekov47@gmail.com>
2026-07-24 11:41:37 +0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './VFES_Talas_App.jsx'
+
+createRoot(document.getElementById('root')).render(React.createElement(App))
+
+setTimeout(() => {
+  const s = document.getElementById('spl')
+  if (s) { s.classList.add('out'); setTimeout(() => s.remove(), 700) }
+}, 3000)
